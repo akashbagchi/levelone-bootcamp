7c1b482ec6c4b1ae5b91be6cdd62dbd12b140ac2
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2021-01-29 13:03:24 +0530

    Update tromboloid.c

--- tromboloid.c
+++ tromboloid.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main()
 {
-	float h, d, b, vol;
+	float h, d, b;
+	double vol;
 	printf("Please enter the dimensions of the tromboloid: \n");
 	printf("h = ");
 	scanf("%f", &h);
@@ -9,7 +10,9 @@ int main()
 	scanf("%f", &d);
 	printf("\nb = ");
 	scanf("%f", &b);
-	vol = (1/3)*((h*d)+d)/b;
-	printf("\nThe volume of a tromboloid with h = %f, d = %f and b = %f is %f",h,d,b,vol);
+	
+	vol = ((h*d)+d)/(3*b);
+	printf("\nThe volume of a tromboloid with h = %f, d = %f and b = %f is %lf",h,d,b,vol);
+	
 	return 0;
 }
