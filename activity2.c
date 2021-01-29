b38535290e4f3c79616bb6bc95db58e015236bc6
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2021-01-29 11:11:18 +0530

    Create activity2.c

---
+++ activity2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+{
+int n1, n2, sum;
+	printf("\nEnter the two numbers to be added: ");
+	scanf("%d, %d", &n1, &n2);
+
+sum = n1+n2;
+printf("\nThe sum is %d", sum);
+} 
