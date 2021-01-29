d7be64569d7a335acd13d32e93e217fb0d1a5ecd
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2021-01-29 16:42:44 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,1 +1,26 @@
-//WAP to find the sum of n fractions.
+#include<stdio.h>
+
+int main()
+{
+	int n, num, den, x,y,gcd;
+	printf("\nEnter how many fractions you want to add: ");
+	scanf("%d",&n);
+	
+	for(int i=0; i<n; i++)
+	{
+printf("\nEnter numerator and denominator: ");
+		scanf("%d %d",&num,&den);
+		if(i==0)
+		{
+			x=num;
+			y=den;
+			continue();
+		}
+		x = (num*y)+(den*x);
+		y = den*y;
+	}
+	
+	printf("\nThe sum of your entered fractions is %d / %d",x,y);
+	return 0;
+
+}
