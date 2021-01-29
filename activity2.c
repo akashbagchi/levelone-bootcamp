d2729f9ff1af2b1fd30d9773f4a609a94d03635f
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2021-01-29 11:12:57 +0530

    Update activity2.c

--- activity2.c
+++ activity2.c
@@ -4,7 +4,7 @@ void main()
 {
 int n1, n2, sum;
 	printf("\nEnter the two numbers to be added: ");
-	scanf("%d, %d", &n1, &n2);
+	scanf("%d %d", &n1, &n2);
 
 sum = n1+n2;
 printf("\nThe sum is %d", sum);
