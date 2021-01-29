3ff6d18c269ff35b1ebf753bf6b64c963e86acd4
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2021-01-29 16:47:23 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -8,17 +8,26 @@ int main()
 	
 	for(int i=0; i<n; i++)
 	{
-printf("\nEnter numerator and denominator: ");
+		printf("\nEnter numerator and denominator: ");
 		scanf("%d %d",&num,&den);
 		if(i==0)
 		{
 			x=num;
 			y=den;
-			continue();
+			continue;
 		}
 		x = (num*y)+(den*x);
 		y = den*y;
 	}
+	
+	for(int j=1; j<x && j<y; j++)
+	{
+		if(x%j==0 && y%j==0)
+			gcd=j;
+	}
+	x = x/gcd;
+	y = y/gcd;
+
 	
 	printf("\nThe sum of your entered fractions is %d / %d",x,y);
 	return 0;
