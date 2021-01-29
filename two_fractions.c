49fe816679a9e889f7ea4970f9a44266f4da5cbc
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2021-01-29 16:10:20 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,1 +1,17 @@
-//WAP to find the sum of two fractions.
+#include<stdio.h>
+
+int main()
+{
+	int n1,d1,n2,d2, n3, d3;
+	int lcm;
+	printf("\nEnter the numerator and denominator for the first fraction :");
+	scanf("%d%d", &n1, &d1);
+	printf("\nEnter the numerator and denominator for the second fraction :");
+	scanf("%d%d", &n2, &d2);
+	n3 = (n1*d2)+(d1*n2);
+	d3 = d1*d2;
+
+
+	printf("\nThe sum of %d / %d and %d / %d is %d / %d", n1,d1,n2,d2,n3,d3);
+	return 0;
+}
