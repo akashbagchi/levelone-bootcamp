9f9f07f17a15eb8f03ad67c3c6606197796e14ce
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2021-01-29 11:22:47 +0530

    Update activity2.c

--- activity2.c
+++ activity2.c
@@ -2,12 +2,12 @@
 
 void main()
 {
-int n1, n2, sum;
+float n1, n2, sum;
 	printf("\nEnter number 1: ");
-	scanf("%d", &n1);
+	scanf("%f", &n1);
 	printf("Enter number 2: ");
-	scanf("%d", &n2);
+	scanf("%f", &n2);
 
 sum = n1+n2;
-printf("\nThe sum of %d and %d is %d",n1,n2, sum);
+printf("\nThe sum of %f and %f is %f",n1,n2, sum);
 } 
