097c6a7c09976a0e252257522efda62fc5031178
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2021-01-30 09:43:54 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,35 +1,63 @@
 #include<stdio.h>
 
-int main()
+struct frac{
+    int num, den;
+}f[50];
+
+void addto(int i, int *x, int *y)
 {
-	int n, num, den, x,y,gcd;
-	printf("\nEnter how many fractions you want to add: ");
-	scanf("%d",&n);
-	
-	for(int i=0; i<n; i++)
+    if(i==0)
 	{
-		printf("\nEnter numerator and denominator: ");
-		scanf("%d %d",&num,&den);
-		if(i==0)
-		{
-			x=num;
-			y=den;
-			continue;
-		}
-		x = (num*y)+(den*x);
-		y = den*y;
+		*x = f[i].num;
+		*y = f[i].den;
 	}
-	
-	for(int j=1; j<x && j<y; j++)
+	else
+	{
+	    *x = (f[i].num * (*y))+(f[i].den * (*x));
+	    *y = f[i].den * (*y);
+
+	}
+}
+
+int gcdcalc(int x, int y)
+{
+    int gcd;
+    for(int j=1; j<x && j<y; j++)
 	{
 		if(x%j==0 && y%j==0)
 			gcd=j;
 	}
+	
+	return gcd;
+}
+
+void output(int x, int y)
+{
+    printf("\nThe sum of your entered fractions is %d / %d",x,y);
+}
+
+void input(int i)
+{
+    printf("\nEnter the numerator and denominator :");
+    scanf("%d %d", &f[i].num, &f[i].den);
+}
+
+int main()
+{
+    int n, x, y, gcd;
+    printf("\nHow many fractions to be added? :");
+    scanf("%d", &n);
+    
+    for(int i=0;i<n;i++)
+    {
+        input(i);
+        addto(i, &x, &y);
+    }
+    
+    gcd = gcdcalc(x,y);
 	x = x/gcd;
 	y = y/gcd;
-
+	output(x,y);
 	
-	printf("\nThe sum of your entered fractions is %d / %d",x,y);
 	return 0;
-
 }
