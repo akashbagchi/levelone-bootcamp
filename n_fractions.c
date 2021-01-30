153b979c80e83a33c8758421abd8104b2b6d9d7e
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2021-01-30 12:01:15 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -8,8 +8,8 @@ void addto(int i, int *x, int *y, struct frac *f)
 {
     	if(i==0)
 	{
-		*x = f->num;
-		*y = f->den;
+	    *x = f->num;
+            *y = f->den;
 	}
 	else
 	{
@@ -21,14 +21,14 @@ void addto(int i, int *x, int *y, struct frac *f)
 
 int gcdcalc(int x, int y)
 {
-    int gcd;
-    for(int j=1; j<x && j<y; j++)
-	{
-		if(x%j==0 && y%j==0)
-			gcd=j;
-	}
-	
-	return gcd;
+    while(x!=y)
+    {
+        if(x>y)
+            x -= y;
+        else
+            y -= x;
+    }
+    return x;
 }
 
 void output(int x, int y)
@@ -42,23 +42,26 @@ void input(int i, struct frac *f)
     scanf("%d %d", &f->num, &f->den);
 }
 
+void calc(int *x, int *y, int gcd)
+{
+    *x = *x/gcd;
+    *y = *y/gcd;
+}
+
 int main()
 {
     struct frac f[50];
     int n, x, y, gcd;
     printf("\nHow many fractions to be added? :");
     scanf("%d", &n);
-    
     for(int i=0;i<n;i++)
     {
         input(i, &f[i]);
         addto(i, &x, &y, &f[i]);
     }
-    
     gcd = gcdcalc(x,y);
-	x = x/gcd;
-	y = y/gcd;
-	output(x,y);
+    calc(&x, &y, gcd);
+    output(x,y);
 	
 	return 0;
 }
