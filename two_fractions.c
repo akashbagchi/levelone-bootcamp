d9f80a20c34cf46b6043f34181fbf395fd44d79f
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2021-01-30 10:50:00 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -2,7 +2,7 @@
 
 struct fraction{
     int n, d;
-}f1, f2;
+};
 
 int gcdcalc(int n3, int d3)
 {
@@ -16,6 +16,7 @@ int gcdcalc(int n3, int d3)
 
 int main()
 {
+    struct fraction f1, f2;
 	int  n3, d3, gcd;
 	int lcm;
 	printf("\nEnter the numerator and denominator for the first fraction :");
@@ -24,8 +25,8 @@ int main()
 	scanf("%d%d", &f2.n, &f2.d);
 	n3 = (f1.n*f2.d)+(f1.d*f2.n);
 	d3 = f1.d*f2.d;
-    	gcd = gcdcalc(n3, d3);
-    	n3 = n3/gcd;
+    gcd = gcdcalc(n3, d3);
+    n3 = n3/gcd;
 	d3 = d3/gcd;
     
 	printf("\nThe sum of %d / %d and %d / %d is %d / %d", f1.n,f1.d,f2.n,f2.d,n3,d3);
